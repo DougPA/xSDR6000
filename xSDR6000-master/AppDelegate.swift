72850f0585751bd8f411e70ed38d94605924b582
Douglas Adams <douglas.adams@me.com>
2018-03-24 15:17:19 -0400

    Initial Commit

---
+++ xSDR6000-master/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xSDR6000-master
+//
+//  Created by Douglas Adams on 3/24/18.
+//  Copyright © 2018 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ xSDR6000-master/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xSDR6000-master
+//
+//  Created by Douglas Adams on 3/24/18.
+//  Copyright © 2018 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
