e28dc236b1da1b8916c216e9661b92fdc586efed
Douglas Adams <douglas.adams@me.com>
2018-11-02 14:09:18 -0400

    Removed pre macOS 10.11 code

--- xSDR6000/CommonClientCode/RadioPicker/Auth0ViewController.swift
+++ xSDR6000/CommonClientCode/RadioPicker/Auth0ViewController.swift
@@ -58,7 +58,7 @@ final class Auth0ViewController             : NSViewController, WKNavigationDele
   @IBOutlet private weak var _customView    : NSView!
   
   private let _api                          = Api.sharedInstance
-  private let _log                          = OSLog(subsystem: "net.k3tzr.xSDR6000", category: "Auth0VC")
+  private let _log                          = OSLog(subsystem: Api.kDomainId + "." + kClientName, category: "Auth0VC")
   private var myWebView                     : WKWebView!
   private let myURL                         = URL(string: smartLinkURL)!
   private let kAutosaveName                 = NSWindow.FrameAutosaveName("AuthViewWindow")
@@ -96,23 +96,14 @@ final class Auth0ViewController             : NSViewController, WKNavigationDele
     _customView.addSubview(myWebView)
     
     // anchor its position
-    if #available(OSX 10.11, *) {
-      // 10.11+
-      [myWebView.topAnchor.constraint(equalTo: _customView.topAnchor),
-       myWebView.bottomAnchor.constraint(equalTo: _customView.bottomAnchor),
-       myWebView.leftAnchor.constraint(equalTo: _customView.leftAnchor),
-       myWebView.rightAnchor.constraint(equalTo: _customView.rightAnchor)].forEach  {
-        anchor in
-        anchor.isActive = true
-      }
-    
-    } else {
-      // before 10.11
-      NSLayoutConstraint(item: myWebView, attribute: .leading, relatedBy: .equal, toItem: _customView, attribute: .leading, multiplier: 1.0, constant: 0.0).isActive = true
-      NSLayoutConstraint(item: myWebView, attribute: .trailing, relatedBy: .equal, toItem: _customView, attribute: .trailing, multiplier: 1.0, constant: 0.0).isActive = true
-      NSLayoutConstraint(item: myWebView, attribute: .top, relatedBy: .equal, toItem: _customView, attribute:.top, multiplier: 1.0, constant:0.0).isActive = true
-      NSLayoutConstraint(item: myWebView, attribute: .bottom, relatedBy: .equal, toItem: _customView, attribute:.bottom, multiplier: 1.0, constant:0.0).isActive = true
+    [myWebView.topAnchor.constraint(equalTo: _customView.topAnchor),
+     myWebView.bottomAnchor.constraint(equalTo: _customView.bottomAnchor),
+     myWebView.leftAnchor.constraint(equalTo: _customView.leftAnchor),
+     myWebView.rightAnchor.constraint(equalTo: _customView.rightAnchor)].forEach  {
+      anchor in
+      anchor.isActive = true
     }
+    
     // load it
     if myWebView.load(request) == nil {
       
